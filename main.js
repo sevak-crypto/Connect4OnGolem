5564011d07e8087cff069fd4dcce4234e591a6f5
sevak-crypto <78248595+sevak-crypto@users.noreply.github.com>
2021-03-24 19:03:40 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+(function() {
+
+	function startGame(ai_1_strength, ai_2_strength) {
+		$('#board').empty();
+
+		var game = new C4({
+			ai_1_strength: ai_1_strength,
+			ai_2_strength: ai_2_strength,
+			container: '#board'
+		});
+	}
+
+
+	$('#players').on('change', function() {
+		var ai_1_strength = parseInt(this.player_1.value, 10);
+		var ai_2_strength = parseInt(this.player_2.value, 10);
+
+		startGame(ai_1_strength, ai_2_strength);
+	}).trigger('change');
+})();
